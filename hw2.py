8546891d8b299dd9296421e94a993ee654bd8a2f
OmerHemo <OmerHemo@users.noreply.github.com>
2019-05-08 17:29:29 +0300

    run contest

--- hw2.py
+++ hw2.py
@@ -9,27 +9,27 @@ CELL_FOUR = 4
 
 def isIn (list,studyProgram):
     for listCell in list:
-        if listCell[CELL_ZERO]==studyProgram:
+        if listCell[CELL_ZERO] == studyProgram:
             return True
     return False
 
 def insertStudyProgramVote (studyProgramsList, studyProgram):
     for listCell in studyProgramsList:
-        if listCell[CELL_ZERO]==studyProgram:
-            listCell[CELL_ONE]+=1
+        if listCell[CELL_ZERO] == studyProgram:
+            listCell[CELL_ONE] += 1
 
 
 def inside_contest(faculty, file_name):
-    file=open(file_name,'r')
-    votersIdList=[]
-    studyProgramsList=[]
+    file = open(file_name,'r')
+    votersIdList = []
+    studyProgramsList = []
     for line in file:
-        listOfLine=line.split(' ')
-        if listOfLine[CELL_ZERO]!="inside":
+        listOfLine = line.split(' ')
+        if listOfLine[CELL_ZERO] != "inside":
             continue
-        elif listOfLine[CELL_ONE]!="contest":
+        elif listOfLine[CELL_ONE] != "contest":
             continue
-        elif listOfLine[CELL_FOUR]!=faculty:
+        elif listOfLine[CELL_FOUR] != faculty:
             continue
         elif listOfLine[CELL_TWO] in votersIdList:
             continue
@@ -38,16 +38,17 @@ def inside_contest(faculty, file_name):
         else:
             studyProgramsList.append([listOfLine[CELL_THREE], 1])
         votersIdList.append(listOfLine[CELL_TWO])
-    maxStudyProgramVotes=0
-    maxStudyProgramName="NULL"
+
+    maxStudyProgramVotes = 0
+    maxStudyProgramName = ""
     for listCell in studyProgramsList:
-        if listCell[1]>maxStudyProgramVotes:
+        if listCell[1] > maxStudyProgramVotes:
             maxStudyProgramVotes=listCell[CELL_ONE]
             maxStudyProgramName=listCell[CELL_ZERO]
-    if maxStudyProgramVotes==0:
-        file.close()
-        return "Error"
+
     file.close()
+    if maxStudyProgramVotes == 0:
+        return "Error"
     return maxStudyProgramName
 
 
